1aea7378ab13e186fc3230d5fa3c805e6f8401fb
teffcode <tefy.1@live.com>
2017-11-06 19:15:42 -0500

    🍏 Single Source of Turth

---
+++ 2_SingleSourceOfTruth/Redux_SingleSourceOfTruth.js
@@ -0,0 +1,41 @@
+const state = [
+    {
+        id: 1,
+        project: 'Mission to Mars',
+        goal: 1000,
+        funded: false
+    },
+    {
+        id: 2,
+        project: 'Mission to Venus',
+        goal: 1000,
+        funded: false
+    }
+]
+
+const stateComplex = {
+    profile: {
+        id: 2,
+        name: 'Tefa',
+        email: 'tefa@linda.com',
+        rating: 5
+    },
+    passangersNearBy: [
+
+    ],
+    notifications: [
+
+    ],
+    complitedRides: [
+
+    ],
+    payments: [
+
+    ],
+    ratings: [
+        {
+            customerId: 5,
+            rating: 4
+        }
+    ]
+}
